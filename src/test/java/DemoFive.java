317af5107d76275795cf90549bfd2e493bc4c702
nik-trifonov <102224540+nik-trifonov@users.noreply.github.com>
2024-03-12 18:37:38 +0200

    Add new test - DemoFive

--- src/test/java/DemoFive.java
+++ src/test/java/DemoFive.java
@@ -1,27 +1,25 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.checkerframework.checker.units.qual.A;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class DemoFiveDragAndDropTwo {
+public class DemoFive{
     public static void main(String[] args) throws Exception {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html");
+        driver.get("https://omayo.blogspot.com");
 
-        Actions actions = new Actions(driver);
+        //accept cookies
+        driver.findElement(By.id("cookieChoiceDismiss")).click();
 
-        WebElement oslo = driver.findElement(By.id("box1"));
-        WebElement norway = driver.findElement(By.id("box101"));
-
-        actions.dragAndDrop(oslo, norway).perform();
-        driver.quit();
-
-
-
+        //create web element
+        WebElement a = driver.findElement(By.linkText("compendiumdev"));
+        Actions action = new Actions(driver);
+        action.moveToElement(a).click().build().perform();
 
     }
 }
